3b208f7ebc5af7b89f8ad39924f994953a68ae35
KittenRoar <yanickvali@gmail.com>
2019-10-02 12:30:14 -0400

    Adjusting error message

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -21,7 +21,7 @@ class UsersController extends AppController
     {
         parent::initialize();
         // Ajoute l'action 'add' à la liste des actions autorisées.
-        $this->Auth->allow(['logout', 'add']);
+        $this->Auth->allow(['logout']);
     }
     public function index()
     {
